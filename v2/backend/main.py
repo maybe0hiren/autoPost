8363e846de143fe22cb25c2fdf72eff6d5299941
maybe0hiren <hirenwaghmare25@gmail.com>
2025-10-21 11:16:37 +0530

    Automated posting

--- v2/backend/main.py
+++ v2/backend/main.py
@@ -12,7 +12,9 @@ load_dotenv()
 geminiKey = os.getenv("GEMINI_API_KEY")
 supabaseURL = os.getenv("SUPABASE_URL")
 supabaseKey = os.getenv("SUPABASE_KEY")
+ayshareKey = os.getenv("AYSHARE_KEY")
 supabase : Client = create_client(supabaseURL, supabaseKey)
+
 
 
 def getCaption(prompt):
@@ -71,14 +73,41 @@ def getPostContent():
 
     print(f"\nDownload complete! {imageCounter-1} images saved in '{today}' folder.")
     print(f"Tasks found: {len(tasks)}")
-
     return tasks
+
+def getDay():
+    with open("day.txt", "r") as f:
+        content = f.read().strip()
+    num = int(content)
+    with open("day.txt", "w") as f:
+        f.write(str(num + 1))
+    return str(num)
+
+def postToX(caption):
+    url = "https://app.ayrshare.com/api/post"
+    headers = {
+        "Authorization": f"Bearer {ayshareKey}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "post": caption,
+        "platforms": ["twitter"]
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(f"{response.json()}\n")
+
+
 
 if __name__ == "__main__":
     tasks = getPostContent()
     caption = "NOTHING TODAY!"
     if(tasks != []):
         tasks = ", ".join(tasks)
-        prompt = "I like journaling about my Computer Science Engineering self learning journey on twitter and instagram and also sometimes about how I am feeling about the day... Keep it breif but well explained caption for the posts. Add hashtags mid sentences, don't bunch them at the end. Most of the times they are about webdev projects and theory, android development projects and theory, Artificial Intelligent projects and theory.... here are the tasks I did today: " + tasks + " The character limit is 280....Just give me 1 final well thought caption and nothing else... No Options, No reasoning nothing else if there are no tasks"
+        prompt = "I like journaling about my Computer Science Engineering self learning journey on twitter and instagram and also sometimes about how I am feeling about the day... Keep it breif but well explained caption for the posts also add one or two relevant emojis. Add hashtags mid sentences, don't bunch them at the end. Most of the times they are about webdev projects and theory, android development projects and theory, Artificial Intelligent projects and theory.... here are the tasks I did today: " + tasks + " The character limit is 230....Just give me 1 final well thought caption and nothing else... No Options, No reasoning nothing else if there are no tasks"
         caption = getCaption(prompt)
-    print(caption)
+        caption = f"Day {getDay()} " + caption
+        postToX(caption)
+        print(f"Posted: {caption} to twitter!")
+    
